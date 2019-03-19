fe29b0ccec810216e17cda9619995071ae1ead5f
Lauri Gustafsson <luutifa@jkry.org>
2019-03-19 17:55:31 +0200

    Add test konstruktoriLaittaaRahaa100000

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -6,11 +6,18 @@ import org.junit.Test;
 
 public class KassapaateTest {
     
+    Kassapaate kp;
+    
     public KassapaateTest() {
     }
 
     @Before
     public void setUp() {
+        kp = new Kassapaate();
     }
     
+    @Test
+    public void konstruktoriLaittaaRahaa100000() {
+        assertEquals(kp.kassassaRahaa(), 100000);
+    }
 }
