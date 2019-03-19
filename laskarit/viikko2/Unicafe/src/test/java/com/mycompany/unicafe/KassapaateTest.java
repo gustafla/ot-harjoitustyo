a11e5d07d1c6796241a689e1fff389604affb8cb
Lauri Gustafsson <luutifa@jkry.org>
2019-03-19 17:28:56 +0200

    Add

---
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -0,0 +1,16 @@
+package com.mycompany.unicafe;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KassapaateTest {
+    
+    public KassapaateTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+    
+}
