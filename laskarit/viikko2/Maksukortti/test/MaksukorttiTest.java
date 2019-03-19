ae92c61c0c8174a8ee681dd8412f5b845b3a7686
Lauri Gustafsson <luutifa@jkry.org>
2019-03-19 16:31:34 +0200

    Add test syoEdullisestiVahentaaSaldoaOikein

--- laskarit/viikko2/Maksukortti/test/MaksukorttiTest.java
+++ laskarit/viikko2/Maksukortti/test/MaksukorttiTest.java
@@ -16,33 +16,43 @@ import static org.junit.Assert.*;
  * @author gustafla
  */
 public class MaksukorttiTest {
-    
+
     public MaksukorttiTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     @Test
-    public void hello() {}
+    public void hello() {
+    }
 
     @Test
     public void konstruktoriAsettaaSaldonOikein() {
         Maksukortti kortti = new Maksukortti(10);
         String vastaus = kortti.toString();
         assertEquals("Kortilla on rahaa 10.0 euroa", vastaus);
+    }
+
+    @Test
+    public void syoEdullisestiVahentaaSaldoaOikein() {
+        Maksukortti kortti = new Maksukortti(10);
+
+        kortti.syoEdullisesti();
+
+        assertEquals("Kortilla on rahaa 7.5 euroa", kortti.toString());
     }
 }
