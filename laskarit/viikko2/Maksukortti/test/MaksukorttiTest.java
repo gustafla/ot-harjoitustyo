68bcc71e03ca559e0a34c5cb0611c29756635d07
Lauri Gustafsson <luutifa@jkry.org>
2019-03-19 16:42:18 +0200

    Do Maksukortti initialization in setUp

--- laskarit/viikko2/Maksukortti/test/MaksukorttiTest.java
+++ laskarit/viikko2/Maksukortti/test/MaksukorttiTest.java
@@ -17,6 +17,8 @@ import static org.junit.Assert.*;
  */
 public class MaksukorttiTest {
 
+    Maksukortti kortti;
+
     public MaksukorttiTest() {
     }
 
@@ -30,6 +32,7 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
+        kortti = new Maksukortti(10);
     }
 
     @After
@@ -42,33 +45,24 @@ public class MaksukorttiTest {
 
     @Test
     public void konstruktoriAsettaaSaldonOikein() {
-        Maksukortti kortti = new Maksukortti(10);
         String vastaus = kortti.toString();
         assertEquals("Kortilla on rahaa 10.0 euroa", vastaus);
     }
 
     @Test
     public void syoEdullisestiVahentaaSaldoaOikein() {
-        Maksukortti kortti = new Maksukortti(10);
-
         kortti.syoEdullisesti();
-
         assertEquals("Kortilla on rahaa 7.5 euroa", kortti.toString());
     }
 
     @Test
     public void syoMaukkaastiVahentaaSaldoaOikein() {
-        Maksukortti kortti = new Maksukortti(10);
-
         kortti.syoMaukkaasti();
-
         assertEquals("Kortilla on rahaa 6.0 euroa", kortti.toString());
     }
 
     @Test
     public void syoEdullisestiEiVieSaldoaNegatiiviseksi() {
-        Maksukortti kortti = new Maksukortti(10);
-
         kortti.syoMaukkaasti();
         kortti.syoMaukkaasti();
         // nyt kortin saldo on 2
