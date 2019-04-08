f20b8878a546a7786935527eb5afb0a821875b05
Lauri Gustafsson <luutifa@jkry.org>
2019-04-08 11:58:11 +0300

    Add Tile enum for map/field

---
+++ TowerDefense/src/main/java/td/domain/Tile.java
@@ -0,0 +1,23 @@
+public enum Tile {
+	WALL(0, 0),
+	ROAD_UP(-1, 0),
+	ROAD_DOWN(1, 0),
+	ROAD_LEFT(0, 1),
+	ROAD_RIGHT(0, -1);
+
+	private int x;
+	private int y;
+
+	public Tile (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int x() {
+		return x;
+	}
+
+	public int y() {
+		return y;
+	}
+}
