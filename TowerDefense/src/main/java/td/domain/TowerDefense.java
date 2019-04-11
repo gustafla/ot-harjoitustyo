acd9e2298bb0b0cb6d1f593416fe2196a89f2c91
Lauri Gustafsson <luutifa@jkry.org>
2019-04-11 16:49:59 +0300

    Add enemy and tower lists in TowerDefense

--- TowerDefense/src/main/java/td/domain/TowerDefense.java
+++ TowerDefense/src/main/java/td/domain/TowerDefense.java
@@ -1,16 +1,24 @@
 package td.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TowerDefense {
 	private int money;
 	private int wave;
 	private int health;
 	private Map map;
 
+	private List<Enemy> enemies;
+	private List<Tower> towers;
+
 	public TowerDefense(Map map) {
 		this.money = 1000;
 		this.wave = 1;
 		this.health = 1000;
 		this.map = map;
+		this.enemies = new ArrayList<>(100);
+		this.towers = new ArrayList<>(20);
 	}
 
 	public int getMoney() {
@@ -29,7 +37,28 @@ public class TowerDefense {
 		return map;
 	}
 
+	public List<Enemy> getEnemies() {
+		return enemies;
+	}
+
+	public List<Enemy> getTowers() {
+		return towers;
+	}
+
+	public void addTower(Tower tower) {
+		towers.add(tower);
+	}
+
 	public void update(double deltaTime) {
+		// spawn enemies
+
+		// check all towers for ready to shoot and find enemy for each in range
+		for (Tower tower: towers) {
+		}
+
+		// move all enemies and check for enemies who have reached the base
+		for (Enemy enemy: enemies) {
+		}
 		
 	}
 }
