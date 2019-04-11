68cb7ee5ef992e1320519b1654fd6261494f4770
Lauri Gustafsson <luutifa@jkry.org>
2019-04-11 16:02:03 +0300

    Add Tower class

---
+++ TowerDefense/src/main/java/td/domain/Tower.java
@@ -0,0 +1,48 @@
+package td.domain;
+
+public class Tower {
+	private int mapPositionX;
+	private int mapPositionY;
+	private double range;
+	private double power;
+	private double cooldown;
+	private double cooldownNow;
+
+	public Tower(int x, int y, double range, double power, double cooldown) {
+		this.mapPositionX = x;
+		this.mapPositionY = y;
+		this.range = range;
+		this.power = power;
+		this.cooldown = cooldown;
+		this.cooldownNow = 0.;
+	}
+
+	public int getMapPositionX() {
+		return mapPositionX;
+	}
+
+	public int getMapPositionY() {
+		return mapPositionY;
+	}
+
+	public double getRange() {
+		return range;
+	}
+
+	public boolean canShoot() {
+		return cooldownNow <= 0.;
+	}
+
+	public void countCooldown(double t) {
+		if (!canShoot()) {
+			cooldownNow -= t;
+		}
+	}
+
+	public void shoot(Enemy enemy) {
+		if (canShoot()) {
+			cooldownNow += cooldown;
+			enemy.takeDamage(power);
+		}
+	}
+}
