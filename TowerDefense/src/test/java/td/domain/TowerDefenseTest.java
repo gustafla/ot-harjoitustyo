f27bb676e937cca3ca4b79b7892502d195d80861
Lauri Gustafsson <luutifa@jkry.org>
2019-04-15 17:16:56 +0300

    Add more tests for TowerDefense

--- TowerDefense/src/test/java/td/domain/TowerDefenseTest.java
+++ TowerDefense/src/test/java/td/domain/TowerDefenseTest.java
@@ -5,11 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TowerDefenseTest {
+	Map map;
 	TowerDefense td;
 
 	@Before
 	public void setUp() {
-		td = new TowerDefense(new Map(0, 0, 0));
+		map = new Map(64, 64, 10, 0);
+		td = new TowerDefense(map);
 	}
 
 	@Test
@@ -25,5 +27,57 @@ public class TowerDefenseTest {
 	@Test
 	public void constructedWithHealth() {
 		assertTrue(0 < td.getHealth());
+	}
+
+	@Test
+	public void constructedWithNoTowers() {
+		assertTrue(td.getTowers().isEmpty());
+	}
+
+	@Test
+	public void constructedWithNoEnemies() {
+		assertTrue(td.getEnemies().isEmpty());
+	}
+
+	@Test
+	public void getMapGivesCorrectMap() {
+		assertEquals(map, td.getMap());
+	}
+
+	@Test
+	public void addTowerWorks() {
+		Tower tower = new Tower(0, 0, 10, 600, 0.2);
+		td.addTower(tower);
+		assertEquals(1, td.getTowers().size());
+		assertTrue(td.getTowers().contains(tower));
+	}
+
+	@Test
+	public void enemiesGetSpawned() {
+		td.nextWave();
+		for (int i = 0; i < 100; i++) {
+			td.update(1./60.);
+		}
+		assertFalse(td.getEnemies().isEmpty());
+	}
+
+	@Test(timeout=1000)
+	public void waveEndsWithNoEnemies() {
+		td.nextWave();
+		while (!td.waveIsOver()) {
+			td.update(1./30.);
+		}
+		assertTrue(td.getEnemies().isEmpty());
+	}
+
+	@Test(timeout=1000)
+	public void waveEndsWithLessHealthAsEnemiesReachBase() {
+		int healthBefore = td.getHealth();
+		td.nextWave();
+		while (!td.waveIsOver()) {
+			td.update(1./30.);
+		}
+		assertTrue(td.getEnemies().isEmpty());
+		assertTrue(td.getHealth() < healthBefore);
 	}
 }
