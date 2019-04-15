61ad3b25f2d274dd68d2d059fd68f52ad3f7dd96
Lauri Gustafsson <luutifa@jkry.org>
2019-04-15 16:58:44 +0300

    Add more tests for Map

--- TowerDefense/src/test/java/td/domain/MapTest.java
+++ TowerDefense/src/test/java/td/domain/MapTest.java
@@ -8,9 +8,11 @@ import org.junit.Test;
 public class MapTest {
 	Map map;
 
+	private final double EPSILON = 0.00001;
+
 	@Before
 	public void setUp() {
-		map = new Map(64, 64, 10, 0);
+		map = new Map(32, 64, 10, 0);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
@@ -41,5 +43,41 @@ public class MapTest {
 	@Test
 	public void type0ContainsWallTiles() {
 		assertEquals(Tile.WALL, map.getTile(0, 0));
+	}
+
+	@Test
+	public void getTileSize() {
+		assertEquals(10, map.getTileSize(), EPSILON);
+	}
+
+	@Test
+	public void getWidth() {
+		assertEquals(64, map.getWidth());
+	}
+
+	@Test
+	public void getHeight() {
+		assertEquals(32, map.getHeight());
+	}
+
+	@Test
+	public void type0BasePositionIsAtBase() {
+		double y = map.getBaseTileY() * map.getTileSize();
+		double x = map.getBaseTileX() * map.getTileSize();
+		assertFalse(map.positionIsAtBase(y + 11, x));
+		assertFalse(map.positionIsAtBase(y + 0.1, x + 11));
+		assertTrue(map.positionIsAtBase(y + 0.1, x + 0.1));
+	}
+
+	@Test
+	public void type0GetTileByPosition() {
+		double y = map.getHeight() * map.getTileSize() * 0.5;
+		double x = map.getWidth() * map.getTileSize() * 0.5;
+		assertEquals(Tile.ROAD_UP, map.getTileByPosition(y, x));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void mapTypeCannotBeNegative() {
+		map = new Map(0, 0, 0, -1);
 	}
 }
