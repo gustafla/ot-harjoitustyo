02e17ab879045a5d5965a1d7b2ef52337727a388
Lauri Gustafsson <luutifa@jkry.org>
2019-04-18 16:57:39 +0300

    Add javadoc for Tile

--- TowerDefense/src/main/java/td/domain/Tile.java
+++ TowerDefense/src/main/java/td/domain/Tile.java
@@ -4,14 +4,23 @@ package td.domain;
  * This enumeration represents the types of an element on the map grid.
  */
 public enum Tile {
+	/** Wall, on which Enemies shouldn't walk on and Towers should be placed on. */
 	WALL(0, 0),
+	/** Road which directs Enemies north. */
 	ROAD_UP(-1, 0),
+	/** Road which directs Enemies south. */
 	ROAD_DOWN(1, 0),
+	/** Road which directs Enemies west. */
 	ROAD_LEFT(0, -1),
+	/** Road which directs Enemies east. */
 	ROAD_RIGHT(0, 1),
+	/** Road which directs Enemies northeast. */
 	ROAD_NE(-1, 1),
+	/** Road which directs Enemies southeast. */
 	ROAD_SE(1, 1),
+	/** Road which directs Enemies southwest. */
 	ROAD_SW(1, -1),
+	/** Road which directs Enemies northwest. */
 	ROAD_NW(-1, -1);
 
 	private int y;
@@ -22,10 +31,20 @@ public enum Tile {
 		this.x = x;
 	}
 
+	/**
+	 * Get the y-component of this Tile's direction for enemies.
+	 *
+	 * @return direction y-component
+	 */
 	public int y() {
 		return y;
 	}
 
+	/**
+	 * Get the x-component of this Tile's direction for enemies.
+	 *
+	 * @return direction x-component
+	 */
 	public int x() {
 		return x;
 	}
